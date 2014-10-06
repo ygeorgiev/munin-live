89a5038042754d6d5bda7939cd068f0622634d35
Dancho <y.georgiev@gmail.com>
2014-10-06 20:33:48 +0300

    select plugin

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $mu = new Munin;
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 <!-- Latest compiled and minified JavaScript -->
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <title>Munin live updater</title>
 </head>
@@ -21,26 +22,29 @@ $mu = new Munin;
       </div>
 
       <div class="jumbotron">
-	<select>
+	<form method="get">
+	<select name="ip" onchange="this.form.submit();">
 	<?php
 	$a = $mu->GetHosts();
 	//var_dump($a);
 	foreach ($a['name'] as $name => $host){
-	  echo "\n\t".'<option value='.$a['ip'][$name].'">'.$host.'</option>';
+	  echo "\n\t".'<option '.($a['ip'][$name]==$_GET['ip'] ? 'selected':''). ' value="'.$a['ip'][$name].'">'.$host.'</option>';
 	}
 	?>
 	</select>
-
-	<select>
 	<?php
-	$p=explode(" ", $mu->GetPlugins($_GET['ip']));
-	var_dump($p);
-	foreach ($p as $id => $plugin){
-		echo "\n\t".'<option value='.$plugin.'">'.$plugin.'</option>';
+	if($_GET['ip'])
+	{
+		echo '<select name="plugin" onchange="this.form.submit();">';
+		$p=explode(" ", $mu->GetPlugins($_GET['ip']));
+		//var_dump($p);
+		foreach ($p as $id => $plugin){
+			echo "\n\t".'<option '.($plugin==$_GET['plugin'] ? 'selected':''). ' value="'.$plugin.'">'.$plugin.'</option>';
+		}
+		echo '</select>';
 	}
 	?>
-	</select>
-
+        </form>
 
         <h1>Graph:</h1>
         <p>.....................................................................................................</p>
